eb9471f24913970fd6f440e71665adcab82ce1f3
Zerawth <Zerawth@10.0.0.2>
2012-09-04 17:29:22 -0500

    added output

--- lalala/src/lalala.java
+++ lalala/src/lalala.java
@@ -1,6 +1,7 @@
 
 public class lalala {
 	public static void main(String[] args) {
-		System.out.println("Hello, World. lalala.");	
+		System.out.println("Hello, World. lalala.");
+		System.out.println("This is weird that things won't work");
 	}
 }
